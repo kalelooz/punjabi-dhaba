184ed23ae08b688b0a248cb5d802557a1a895f39
Mohamed Elkhalil Eloued <elouedm@aljazeera.net>
2025-06-10 10:46:45 +0300

    Fix website asset paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,31 @@
 /** @type {import('next').NextConfig} */
+
+// This file configures the "address" for your website on GitHub Pages.
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  // Trim preceding slash since NEXT_PUBLIC_BASE_PATH starts with a slash
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+
 const nextConfig = {
   output: 'export',
-
   // This tells Next.js the full "address" for your website on GitHub Pages.
-  basePath: '/punjabi-dhaba', 
-  assetPrefix: '/punjabi-dhaba/',
+  basePath: basePath, 
+  assetPrefix: assetPrefix,
 
-  // This tells Next.js it's allowed to load images from these websites.
+  // This tells Next.js it's allowed to load images from these websites
+  // and disables its default image optimization, which is required for GitHub Pages.
   images: {
-    unoptimized: true, // Required for static export on GitHub pages
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
